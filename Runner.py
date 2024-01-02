41f609c6b25e11f4ddc20d952c54977593388f0f
Julian Czarnecki <jczarnecki@student.agh.edu.pl>
2024-01-02 15:33:15 +0100

    Adding ZGEEV diagonalization

--- Runner.py
+++ Runner.py
@@ -8,7 +8,7 @@ import sys
 def LAO_STO_default_nml():
     parser = f90nml.Parser()
     params_nml = parser.reads(f'&physical_params \
-                                    T = 0.01, \
+                                    T = 0.00001, \
                                     t_D = 0.5e3, \
                                     t_I = 0.04e3, \
                                     lambda_SOC = 0.01e3, \
@@ -16,14 +16,14 @@ def LAO_STO_default_nml():
                                     v = 0.2e3, \
                                     V_pdp = 0.028e3, \
                                     V_pds = -0.065e3, \
-                                    J_SC = 0.165e3, \
-                                    J_SC_PRIME = 0.0165e3, \
+                                    J_SC = 0.175e3, \
+                                    J_SC_PRIME = 0.0175e3, \
                                     U_HUB = 2e4, \
                                     V_HUB = 2e4, \
                                     E_Fermi = -0.5e3 / \
                                 &discretization \
-                                    k1_steps = 5000, \
-                                    k2_steps = 5000 / \
+                                    k1_steps = 8000, \
+                                    k2_steps = 8000 / \
                                 &self_consistency \
                                     max_sc_iter = 1000, \
                                     sc_alpha = 0.1, \
@@ -50,10 +50,16 @@ def run_single_Ef(E_Fermi):
 
 if __name__ == '__main__':
     Ef_min = -0.7e3
-    Ef_max = -0.4e3
-    Ef_steps = 30
+    Ef_max = 0.7e3
+    Ef_steps = 100
     dE = abs(Ef_max - Ef_min)/Ef_steps
     Fermi_table = [Ef_min + i*dE for i in range(Ef_steps + 1)]
     
-    with multiprocessing.Pool(processes=8) as pool:
-        pool.map(run_single_Ef, Fermi_table)
+    try:
+        with multiprocessing.Pool(processes=8) as pool:
+            pool.map(run_single_Ef, Fermi_table)
+    except KeyboardInterrupt:
+        print("Ctrl+C detected. Terminating processes.")
+        pool.terminate()
+        pool.join()
+        print("Processes terminated.")
--- mod_writers.f90
+++ mod_writers.f90
@@ -27,7 +27,7 @@ SUBROUTINE PRINT_HAMILTONIAN(Hamiltonian, filename)
 END SUBROUTINE PRINT_HAMILTONIAN
 
 SUBROUTINE PRINT_ENERGIES(Energies, k1_steps, k2_steps, dk1, dk2, filename)
-    REAL*8, INTENT(IN) :: Energies(-k1_steps:k1_steps, -k2_steps:k2_steps, DIM)
+    REAL*8, INTENT(IN) :: Energies(0:k1_steps, 0:k2_steps, DIM)
     REAL*8, INTENT(IN) :: dk1, dk2
     INTEGER*4, INTENT(IN) :: k1_steps, k2_steps
 
@@ -41,7 +41,7 @@ SUBROUTINE PRINT_ENERGIES(Energies, k1_steps, k2_steps, dk1, dk2, filename)
     DO n = 1, DIM
         DO i = 0,k1_steps
             DO j = 0, k2_steps
-                WRITE(9,*) i*dk1/SQRT(3.) * A_TILDE, (-i*dk1/3. + 2./3.*j*dk2) * A_TILDE, Energies(i, j, n)/meV2au*1e-3
+                WRITE(9,*) i*dk1*SQRT(3.)/2. * A_TILDE, ( -i*dk1/2. + j*dk2 ) * A_TILDE, Energies(i, j, n)/meV2au*1e-3
             END DO
         END DO
         WRITE(9,*)
