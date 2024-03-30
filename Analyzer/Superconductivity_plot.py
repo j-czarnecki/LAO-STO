9a320a97f000e6e51df4a98445b81f7f6d00f81b
Julian Czarnecki <jczarnecki86@gmail.com>
2024-03-30 20:29:06 +0100

    Adding symmetry checks in Gamma. Adding analyzer testing.

--- Analyzer/Superconductivity_plot.py
+++ Analyzer/Superconductivity_plot.py
@@ -115,13 +115,39 @@ class DataReader:
                 continue
             firstIter = False
 
+
+
+#This class should be improved
 class SymmetryResolver:
     
-    def __init__(self):
-        self.symmetryGamma = {}
-    
-    def SWavePairing(self):
-        pass
+    def __init__(self, nNeighbors):
+        self.nNeighbors = nNeighbors
+        self.symmetryGammaDict = {}
+
+    #RETHINK ___WavePairing construction to avoid code repetition
+    def _SWavePairing(self, listOfGammas: list):
+        p = 0
+        M = 0
+        return self._CalculateSingleGamma(listOfGammas, p, M)
+
+    def _PWavePairing(self, listOfGammas: list):
+        p = 1
+        M = 1
+        return self._CalculateSingleGamma(listOfGammas, p, M)
+
+    def _DWavePairing(self, listOfGammas: list):
+        p = 0
+        M = 2
+        return self._CalculateSingleGamma(listOfGammas, p, M)
+
+    def _CalculateSingleGamma(self, listOfGammas: list, p: int, M: int):
+        symmetryGamma = 0
+        for i in range(self.nNeighbors):
+            currentPhase = 2*np.pi / self.nNeighbors * i
+            phaseFactor = np.exp(-1j*M*currentPhase)
+            symmetryGamma += listOfGammas[i]*phaseFactor
+
+        return symmetryGamma*(1j)**p/self.nNeighbors
 
     def CalculateSymmetryGamma(self, gammaDict):
 
@@ -132,12 +158,35 @@ class SymmetryResolver:
 
                     #Loop over all gammas for given spin, sublat and orbital
                     for i in range(len(gammaDict[(1,1,1,1)])):
-                        symmetryKey = (spin, sublat, orbital)
+                        gammaToSymmetrize = []
+                        for neighbor in range(1, self.nNeighbors + 1):
+                            gammaToSymmetrize.append(gammaDict[(spin, neighbor, sublat, orbital)][i])
                         if i == 0:
-                            self.symmetryGamma[symmetryKey] = self.SWavePairing(symmetryKey, gammaDict)
+                            self.symmetryGammaDict[(spin,sublat,orbital, 's')] = [self._SWavePairing(gammaToSymmetrize)]
+                            self.symmetryGammaDict[(spin,sublat,orbital, 'p')] = [self._PWavePairing(gammaToSymmetrize)]
+                            self.symmetryGammaDict[(spin,sublat,orbital, 'd')] = [self._DWavePairing(gammaToSymmetrize)]
                         else:
-                            self.symmetryGamma[symmetryKey].append(self.SWavePairing(symmetryKey, gammaDict))
+                            self.symmetryGammaDict[(spin,sublat,orbital, 's')].append(self._SWavePairing(gammaToSymmetrize))
+                            self.symmetryGammaDict[(spin,sublat,orbital, 'p')].append(self._PWavePairing(gammaToSymmetrize))
+                            self.symmetryGammaDict[(spin,sublat,orbital, 'd')].append(self._DWavePairing(gammaToSymmetrize))
 
+
+def testing():
+    symmetryResolver = SymmetryResolver(3)
+
+    testGammaDict = {}
+    for spin in [1,2]:
+        for sublat in [1,2]:
+            for orbital in [1,2,3]:
+                for neighbor in [1,2,3]:
+                    key = (spin, neighbor, sublat, orbital)
+                    testGammaDict[key] = [x*neighbor for x in range(1,3)]
+
+    print(testGammaDict)
+    symmetryResolver.CalculateSymmetryGamma(testGammaDict)
+    print(symmetryResolver.symmetryGammaDict[(1,1,1,'s')])
+    print(symmetryResolver.symmetryGammaDict[(1,1,1,'p')])
+    print(symmetryResolver.symmetryGammaDict[(1,1,1,'d')])
 
 def main():
 
@@ -179,4 +228,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    #main()
+    testing()
